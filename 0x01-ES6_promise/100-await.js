35ba793f19c8a8734d1a6e3ac1e5606155aff44f
teddyomondi <theoriginalsomosh@gmail.com>
2024-04-28 10:42:57 +0300

    Created an async function to upload a photo and create a user, returning an object with their responses.

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,17 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
-async function asyncUploadUser() {
-    try {
-        const photoResponse = await uploadPhoto();
-        const userResponse = await createUser();
+export default async function asyncUploadUser() {
+  let res = {};
 
-        return {
-            photo: photoResponse,
-            user: userResponse,
-        };
-    } catch (error) {
-        return {};
-    }
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
+  }
+  return res;
 }
-
-export default asyncUploadUser;
