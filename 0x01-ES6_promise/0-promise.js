041fd6ee3de24e7750908fcadac92fe234c9a132
teddyomondi <theoriginalsomosh@gmail.com>
2024-04-28 09:41:26 +0300

    "Add Task Create a Promise and check if it's a Promise instance.

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,3 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Perform API request or any asynchronous operation here
-        // For simplicity, we'll resolve the Promise immediately
-        resolve("Response from API");
-    });
+export default function getResponseFromAPI() {
+	return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
